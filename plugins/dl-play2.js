7351ec036e3179fbde8cb9206240bea65e5211a9
CYBER WARRIOR <134825125+CYBER-WARRIIOR@users.noreply.github.com>
2024-07-29 18:08:30 +0500

    𝑪𝒀𝑩𝑬𝑹_𝑾𝑨𝑹𝑹𝑰𝑶𝑹

--- plugins/dl-play2.js
+++ plugins/dl-play2.js
@@ -31,7 +31,7 @@ const handler = async (m, {conn, command, args, text, usedPrefix}) => {
   ⏰ Duration: ${secondString(yt_play[0].duration.seconds)}
   🎴 Views: ${yt_play[0].views}
   🔗 Link: ${yt_play[0].url}
-⊱─━⊱༻ⓅⓇⒾⓃⒸⒺⒷⓄⓉ༺⊰━─⊰`;
+─━༻𝑪𝒀𝑩𝑬𝑹_𝑾𝑨𝑹𝑹𝑰𝑶𝑹༺⊰━─`;
     conn.sendMessage(m.chat, {image: {url: yt_play[0].thumbnail}, caption: texto1}, {quoted: m});
     if (command == 'play') {
     try {
